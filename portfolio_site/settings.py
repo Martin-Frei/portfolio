e5a5c2f34ae6d68a8178fd46f75e077855fa6cd3
Martin Frei <mat.frei@gmx.de>
2025-11-16 21:01:45 +0100

    Fix STATIC_ROOT for PythonAnywhere

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -119,11 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/MatFRei/portfolio/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
 
 # Für später (Production):
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
