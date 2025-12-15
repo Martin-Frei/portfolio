26132603b6ee1225a33e8eb7482eff8b3fd6fd5a
Martin Frei <mat.frei@gmx.de>
2025-12-15 16:02:47 +0100

    fix: MEDIA_ROOT auf Railway Volume /app/media

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -126,7 +126,8 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+# MEDIA_ROOT = BASE_DIR / "media" # nur benötigt für Local File Uploads
+MEDIA_ROOT = '/app/media'  # ← dein Volume-Mount-Path auf Railway
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
